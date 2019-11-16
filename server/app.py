24f11712f47342a4ef1c71f1db34d6af0f16cb30
shakandrew <shakandrew1996@gmail.com>
2019-11-16 23:43:56 +0100

    GreenList controller implementation + API part

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
 import json
-from flask import Flask, abort, request
+from flask import Flask, abort, request, make_response
 from flask_restful import Api, Resource
 from controllers.challenge import ChallengeController
+from controllers.greenlist import GreenListController
 from controllers.suggestion import SuggestionController
 from controllers.product import ProductController
 
@@ -13,17 +14,18 @@ api = Api(app)
 class Challenge(Resource):
     def get(self):
         try:
-            data = request.get_json()
-            response = ChallengeController.get_challenge(data["uuid"])
-            return json.dumps(response)
+            uuid = request.headers["GreenList-User-Id"]
+            response = ChallengeController.get_challenge(uuid)
+            return make_response(json.dumps(response))
         except Exception as e:
             pass
 
     def post(self):
         try:
-            data = request.get_json()
-            response = ChallengeController.create_challenge(data['uuid'], data['duration'])
-            return json.dumps(response)
+            uuid = request.headers["GreenList-User-Id"]
+            duration = request.get_json()['duration']
+            response = ChallengeController.create_challenge(uuid, duration)
+            return make_response(json.dumps(response))
         except Exception as e:
             pass
 
@@ -31,32 +33,38 @@ class Challenge(Resource):
 class GreenList(Resource):
     def get(self):
         try:
-            pass
+            uuid = request.headers["GreenList-User-Id"]
+            response = GreenListController.get_list(uuid)
+            return make_response(json.dumps(response))
         except Exception as e:
             pass
 
     def post(self):
         try:
-            pass
+            uuid = request.headers["GreenList-User-Id"]
+            product_id = request.get_json()["productId"]
+            response = GreenListController.add_to_list(uuid, product_id)
+            return make_response(json.dumps(response))
         except Exception as e:
             pass
 
 
 class ProductSuggestions(Resource):
     def get(self, product_id):
-        try:
-            session = Session()
-            product = ProductController().get_product(product_id, session)
-            if not product:
-                abort(404)
-            suggestions = SuggestionController().get_suggestions(
-                    product, session)
-
-
-        except Exception as e:
-            pass
-        finally:
-            session.close()
+        # try:
+        #     session = Session()
+        #     product = ProductController().get_product(product_id, session)
+        #     if not product:
+        #         abort(404)
+        #     suggestions = SuggestionController().get_suggestions(
+        #             product, session)
+        #
+        #
+        # except Exception as e:
+        #     pass
+        # finally:
+        #     session.close()
+        pass
 
 
 class Purchase(Resource):
