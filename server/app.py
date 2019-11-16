78ad433e76a8cde563222ca33c602586d8f8a621
shakandrew <shakandrew1996@gmail.com>
2019-11-16 18:35:07 +0100

    API initialization, DB model added

--- server/app.py
+++ server/app.py
@@ -1,12 +1,38 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 
+api = Api(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
 
+class Challenge(Resource):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+
+class GreenList(Resource):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+
+class ProductSuggestions(Resource):
+    def get(self):
+        pass
+
+
+class Purchase(Resource):
+    def post(self):
+        pass
+
+
+api.add_resource(Challenge)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
