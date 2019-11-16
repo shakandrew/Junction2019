01f6fba0909d8e96063df7e3434a44fe130a5bba
Pavel Senchanka <pavel.senchanka@gmail.com>
2019-11-17 00:34:37 +0200

    More work on suggestions

--- server/app.py
+++ server/app.py
@@ -1,9 +1,10 @@
 import json
-from flask import Flask, abort, request
+from flask import Flask, request
 from flask_restful import Api, Resource
 from controllers.challenge import ChallengeController
 from controllers.suggestion import SuggestionController
 from controllers.product import ProductController
+from model import Session
 
 app = Flask(__name__)
 
@@ -43,20 +44,43 @@ class GreenList(Resource):
 
 
 class ProductSuggestions(Resource):
-    def get(self, product_id):
+    def get(self):
         try:
             session = Session()
-            product = ProductController().get_product(product_id, session)
-            if not product:
-                abort(404)
-            suggestions = SuggestionController().get_suggestions(
-                    product, session)
+            products = (
+                    SuggestionController().get_suboptimal_products(session))
+            result = []
+            for product in products:
+                product_json = self.product_to_json(product)
 
+                bigger_sizes, same_class = (
+                        SuggestionController()
+                        .get_suggestions(product, session))
 
-        except Exception as e:
-            pass
+                alternatives = []
+                alternatives.extend([
+                    self.product_to_json(product)
+                    for product in same_class])
+                alternatives.extend([
+                    self.product_to_json(product, multiple)
+                    for (product, multiple) in bigger_sizes])
+
+                product_json.update({'alternatives': alternatives})
+                result.append(product_json)
+            return result
         finally:
             session.close()
+
+    def product_to_json(self, product, multiple=None):
+        result = {
+                'id': product.id,
+                'name': product.name,
+                'picture': 'STUB',
+                'footprint': product.footprint,
+        }
+        if multiple:
+            result.update({'multiple': multiple})
+        return result
 
 
 class Purchase(Resource):
@@ -69,7 +93,7 @@ class Purchase(Resource):
 
 api.add_resource(Challenge, '/challenge')
 api.add_resource(GreenList, '/greenList')
-api.add_resource(ProductSuggestions, '/productSuggestions/<product_id>')
+api.add_resource(ProductSuggestions, '/productSuggestions')
 api.add_resource(Purchase, '/purchases')
 
 if __name__ == '__main__':
