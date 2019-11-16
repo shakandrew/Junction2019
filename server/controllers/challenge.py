c889e0e095be95439c229d916fff3fc9dce5a2c8
shakandrew <shakandrew1996@gmail.com>
2019-11-16 23:41:02 +0100

    Challenge controller bug fixes

--- server/controllers/challenge.py
+++ server/controllers/challenge.py
@@ -13,8 +13,8 @@ class ChallengeController:
         if user is None:
             raise Exception("There is no user [id={}]".format(uuid))
         resp = {
-            "startTime": user.challenge_start_ts,
-            "endTime": user.challenge_end_ts,
+            "startTime": str(user.challenge_start_ts),
+            "endTime": str(user.challenge_end_ts),
             "score": user.challenge_score
         }
         session.close()
@@ -22,14 +22,19 @@ class ChallengeController:
 
     @staticmethod
     def create_challenge(uuid, duration):
-        session = Session()
-        user = User(
-            greenlist_uuid=uuid,
-            challenge_start_ts=datetime.utcnow(),
-            challenge_end_ts=datetime.utcnow() + timedelta(days=duration * 30),
-            challenge_score=0
-        )
-        session.add(user)
-        session.commit()
-        session.close()
-        return "Success"
+        msg = "Success"
+        try:
+            session = Session()
+            user = User(
+                greenlist_uuid=uuid,
+                challenge_start_ts=datetime.utcnow(),
+                challenge_end_ts=datetime.utcnow() + timedelta(days=duration * 30),
+                challenge_score=0
+            )
+            session.add(user)
+            session.commit()
+            session.close()
+        except Exception as e:
+            msg = e
+        finally:
+            return msg
---
+++ server/controllers/greenlist.py
@@ -0,0 +1,36 @@
+from model import Session, UserList, User, Product
+
+
+class GreenListController:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_list(uuid):
+        session = Session()
+        resp = []
+        for elem in session.query(UserList).filter(User.greenlist_uuid == uuid).all():
+            resp.append({
+                "productId": elem.product.id,
+                "name": elem.product.name,
+                "footprint": elem.product.footprint,
+                "image": elem.product.image
+            })
+        session.close()
+        return resp
+
+    @staticmethod
+    def add_to_list(uuid, product_id):
+        msg = "Success"
+        try:
+            session = Session()
+            user = session.query(User).filter(User.greenlist_uuid == uuid).one_or_none()
+            product = session.query(Product).filter(Product.id == product_id).one_or_none()
+            product_to_user = UserList(user, product)
+            session.add(product_to_user)
+            session.commit()
+            session.close()
+        except Exception as e:
+            msg = e
+        finally:
+            return msg
