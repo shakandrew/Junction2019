3524a00c8466a43c1ec196482ea9a00ea099c883
shakandrew <shakandrew1996@gmail.com>
2019-11-16 22:31:22 +0100

    Challenge functionality complete

--- server/app.py
+++ server/app.py
@@ -1,5 +1,7 @@
+import json
 from flask import Flask, request
 from flask_restful import Api, Resource
+from controllers.challenge import ChallengeController
 
 app = Flask(__name__)
 
@@ -8,31 +10,56 @@ api = Api(app)
 
 class Challenge(Resource):
     def get(self):
-        pass
+        try:
+            data = request.get_json()
+            response = ChallengeController.get_challenge(data["uuid"])
+            return json.dumps(response)
+        except Exception as e:
+            pass
 
     def post(self):
-        pass
+        try:
+            data = request.get_json()
+            response = ChallengeController.create_challenge(data['uuid'], data['duration'])
+            return json.dumps(response)
+        except Exception as e:
+            pass
 
 
 class GreenList(Resource):
     def get(self):
-        pass
+        try:
+            pass
+        except Exception as e:
+            pass
 
     def post(self):
-        pass
+        try:
+            pass
+        except Exception as e:
+            pass
 
 
 class ProductSuggestions(Resource):
     def get(self):
-        pass
+        try:
+            pass
+        except Exception as e:
+            pass
 
 
 class Purchase(Resource):
     def post(self):
-        pass
+        try:
+            pass
+        except Exception as e:
+            pass
 
 
-api.add_resource(Challenge)
+api.add_resource(Challenge, '/challenge')
+api.add_resource(GreenList, '/greenList')
+api.add_resource(ProductSuggestions, '/productSuggestions')
+api.add_resource(Purchase, '/purchases')
 
 if __name__ == '__main__':
     app.run(debug=True)
